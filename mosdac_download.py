8ad6a799467ce02a015b4f2c7038801410747561
THE-DEEPDAS <deepdblm@gmail.com>
2025-07-19 22:25:43 +0530

    Removing credentials

--- mosdac_download.py
+++ mosdac_download.py
@@ -3,8 +3,8 @@ import os
 
 MOSDAC_HOST = 'download.mosdac.gov.in'
 MOSDAC_PORT = 22
-USERNAME = 'deep_das'  
-PASSWORD = 'BdS@1234' 
+USERNAME = 'yaha daalo username'  
+PASSWORD = 'yaha password daalo' 
 REQUEST_ID = 'Jul2025_137485'      
 LOCAL_DOWNLOAD_DIR = 'mosdac'  
 cnopts = pysftp.CnOpts()
@@ -20,27 +20,27 @@ with pysftp.Connection(host=MOSDAC_HOST, username=USERNAME, password=PASSWORD, p
         subfolders = [f for f in sftp.listdir() if sftp.isdir(f)]
         
         if not subfolders:
-            print("❌ No subfolders found in the /Order directory.")
+            print("No subfolders found in the /Order directory.")
         else:
             # Sort subfolders and pick the last one
             subfolders.sort()
             last_folder = subfolders[-1]
-            print(f"📂 Last folder found: {last_folder}")
+            print(f"Last folder found: {last_folder}")
             
             sftp.cwd(last_folder)
             os.makedirs(LOCAL_DOWNLOAD_DIR, exist_ok=True)
             files = sftp.listdir()
-            print(f"📦 Total files found in {last_folder}: {len(files)}")
+            print(f"Total files found in {last_folder}: {len(files)}")
             
             for file in files:
                 local_path = os.path.join(LOCAL_DOWNLOAD_DIR, file)
                 try:
-                    print(f"⬇ Downloading: {file}...")
+                    print(f"Downloading: {file}...")
                     sftp.get(file, local_path)
-                    print(f"✅ Successfully downloaded: {file}")
+                    print(f"Successfully downloaded: {file}")
                 except Exception as e:
-                    print(f"❌ Failed to download {file}: {e}")
+                    print(f"Failed to download {file}: {e}")
             
-            print("✅ All files processed.")
+            print("All files processed.")
     else:
-        print(f"❌ Folder not found: {request_folder}")
+        print(f"Folder not found: {request_folder}")
